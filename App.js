76a345f43bbb63b0476bf4a4357e95f5655470cd
Furqan Hameed <furqancode@gmail.com>
2023-02-12 11:15:47 +0530

    Initial app

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import Torch from 'react-native-torch';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [isTorchOn, setIsTorchOn] = useState(false);
+
+	const handleTouch = () => {
+		Torch.switchState(!isTorchOn);
+		setIsTorchOn(!isTorchOn);
+	}
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={handleTouch}>
+                <View style={styles.button}>
+                    <Text style={styles.text}>{isTorchOn ? "OFF" : "ON"}</Text>
+                </View>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#26272B",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    button: {
+        borderWidth: 10,
+        borderColor: "#8cad06",
+        backgroundColor: "#AFD808",
+        padding: 30,
+    },
+    text: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 50,
+    },
 });
